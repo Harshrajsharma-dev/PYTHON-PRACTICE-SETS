ee0c3cced9906c07f765077a078ccde9dcd8dd1b
Harshrajsharma-dev <harshrajsharma085@gmail.com>
2025-06-22 06:32:37 +0530

    Add files via upload
    EXCERCISE 1 ON THE BASICS OF PYTHON TAKING INPUTS OF NO. , TAKING SQAURES, CUBES, AVERAGE , ETC.

---
+++ EXCERCISE-1/Redame.md
@@ -0,0 +1,7 @@
+🐍 Beginner Python Programs
+This repository contains a collection of simple Python programs aimed at helping beginners understand the basics of Python programming. Each script covers a fundamental concept, such as:
+- Taking user input
+- Performing arithmetic operations (addition, modulus, square, cube, average)
+- Working with data types
+- Using comparison operators
+These examples serve as a great starting point for anyone new to programming and looking to develop a strong foundation in Python. Whether you're a student practicing coding exercises or someone brushing up on basics, this repository can help you get hands-on with essential logic and syntax.
---
+++ EXCERCISE-1/practiceset2.py
@@ -0,0 +1,32 @@
+#Write a python program to add two numbers.
+'''a=int(input("Enter a number: "))
+b=int(input("Enter another number: "))
+print("The sum of the two numbers is:", a + b)'''
+
+
+#Write a python program to find remainder when a number is divided by 2. 
+'''a = int(input("Enter a number: "))
+print("The remainder is=", a % 2)'''
+
+#Write a python program to find the type of input given by user.
+'''a=int(input("Enter anything: "))
+print("The type of the input is:", type(a))'''
+
+
+#use comparison operators to find out wherher a given variable 'a' is greater than 'b' or not . Take a=34 and b=80.
+'''a=34
+b=80
+if a > b:
+    print("a is greater than b")
+else:
+    print("b is greater than a")'''
+
+#Write a python program to find the average of two numbers.
+'''a=int(input("Enter a number: "))
+b=int(input("Enter another number: "))
+print("The average of two no. is", a+b/2)'''
+
+#Write a python program to find the square of a number.
+a=int(input("Enter a number: "))
+print("The square of the number is:", a*a)
+
