bdd99cde42fec7a18e104ea8ba88fd756a1b6db8
Harshrajsharma-dev <harshrajsharma085@gmail.com>
2025-06-23 21:29:44 +0530

    Add files via upload
    Coded this after learming recursion in python.

---
+++ Exercise-3- Factorial/Index.py
@@ -0,0 +1,26 @@
+# Name=str(input("Enter your name: "))
+# Country=str(input("Enter your country: "))
+# print(f"Hello my name is {Name}, and I am from {Country}.")
+
+
+# PROGRAM ON FACTORIAL. BY USING RECURSION METHOD.
+
+# def factorial(n):
+#     if (n == 0 or n == 1):
+#         return 1
+#     else:
+#         return n * factorial(n-1)
+
+# n=int(input("Enter a number to calculate its factorial: "))
+# print(f"The factorial of your no. is {factorial(n)}")
+
+# FACTORIAL USING ITERATION
+
+# num=int(input("Enter a number to calculate its factorial: "))
+# factorial = 1
+# if (num == 0 or num == 1):
+#     print(1)
+# else:
+#     for i in range(1, num + 1):
+#         factorial *= i
+#     print(f"The factorial of {num} is {factorial}")
